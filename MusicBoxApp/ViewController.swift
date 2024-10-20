8594e3614dee6a284ba2920c44555ae3f2a465f8
darkCavalier11 <sumit.kumar@changepay.in>
2024-10-20 09:52:57 +0530

    Initial Commit

---
+++ MusicBoxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicBoxApp
+//
+//  Created by Sumit Pradhan on 20/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ MusicBoxAppTests/MusicBoxAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MusicBoxAppTests.swift
+//  MusicBoxAppTests
+//
+//  Created by Sumit Pradhan on 20/10/24.
+//
+
+import Testing
+@testable import MusicBoxApp
+
+struct MusicBoxAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
