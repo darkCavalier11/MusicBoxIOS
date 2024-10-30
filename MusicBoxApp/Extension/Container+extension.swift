0a786688d968766af9f178a704c1cce7159ffefd
darkCavalier11 <sumit.kumar@changepay.in>
2024-10-30 11:11:00 +0530

    initialised DI container

--- MusicBoxApp/Extension/Container+extension.swift
+++ MusicBoxApp/Extension/Container+extension.swift
@@ -9,12 +9,17 @@ import Swinject
 import MusicBox
 
 extension Container {
-  static var shared: Container = {
+  static var sharedContainer: Container = {
     let container = Container()
     container.register(CoreDataStack.self) { _ in
       CoreDataStack()
     }
-    
+    container.register(MusicBox.self) { _ in
+      MusicBox()
+    }
+    container.register(PlayingViewModel.self) { _ in
+      MusicPlayingViewModel()
+    }
     return container
   }()
 }
